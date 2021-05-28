04e07592b71b0045377b6de58cb4b7ce101ffb63
Juan Carlos Diaz <judiazp2p@gmail.com>
2021-05-28 15:22:03 -0300

    desplegando GifExpertApp a produccion

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,6 @@
 
 export const getGifs = async( category ) => {
+    
     const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=20&api_key=r4Gh4kFBn2bOQtHLdl3C6bYEWGIwaWtB`
     const resp = await fetch(url);
     const { data } = await resp.json();
