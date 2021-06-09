edfc0a9e23e6d22a7bf05726b7b5f7047ccfd13d
Juan Carlos Diaz <judiazp2p@gmail.com>
2021-06-09 17:46:51 -0300

    implementando dark mode

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,14 +5,24 @@ import GifGrid from './components/gifGrid';
 const GiffExpertApp = () => {
 
     const [categories, setCategories] = useState(['']);
+    const [darkMode, setDarkMode] = useState(false)
+
+    const handleClick = () => {
+        setDarkMode(!darkMode)
+        document.body.classList.toggle('dark')
+    }
 
     return (
         <div id="addCategory" className="contenedor">
             <nav>
-                <a class="logotipo" href="#">GiffExpertApp</a>
+                <a class="logotipo" href="/">GiffExpertApp</a>
                 <div>
                     <AddCategory setCategories={ setCategories }/>
                 </div>
+                <button className={ darkMode? "switch active" : "switch" } onClick={ () => handleClick()}> 
+                    <span> <i class="fas fa-sun"></i> </span>
+                    <span> <i class="fas fa-moon"></i> </span>
+                </button>
             </nav>
             {
                 categories.map(category => <GifGrid key={category} category={ category }/>)
